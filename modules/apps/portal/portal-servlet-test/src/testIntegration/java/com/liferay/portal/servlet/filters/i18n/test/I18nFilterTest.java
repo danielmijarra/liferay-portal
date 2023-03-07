4920cc9efc1ffa006887313ae9ce66b3e9d4e8e5
DanielMijarra <59836009+danielmijarra@users.noreply.github.com>
2023-03-07 09:56:51 +0100

    LPS-173562 I18nFilter Test

--- modules/apps/portal/portal-servlet-test/src/testIntegration/java/com/liferay/portal/servlet/filters/i18n/test/I18nFilterTest.java
+++ modules/apps/portal/portal-servlet-test/src/testIntegration/java/com/liferay/portal/servlet/filters/i18n/test/I18nFilterTest.java
@@ -26,7 +26,7 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.servlet.filters.i18n.I18nFilter;
+import com.liferay.portal.i18n.filter.internal.I18nFilter;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
